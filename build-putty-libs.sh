7c41a912d85b8aaabe1907bd1673c8ae4bf97beb
Rytis Petronis <petronisrytis@gmail.com>
2023-02-10 02:18:47 +0000

    Add cross compilation to unix and wasm, add correct targets for the dlls for the nuget

---
+++ build-putty-libs.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+./dockcross-web-wasm make clean -f ./WASM/Makefile  -C ./Putty
+./dockcross-web-wasm make -f ./WASM/Makefile  -C ./Putty
+./dockcross-linux-x64 make clean -f ./UNIX/Makefile  -C ./Putty
+./dockcross-linux-x64 make -f ./UNIX/Makefile  -C ./Putty
+./dockcross-linux-x86 make clean -f ./UNIXx86/Makefile  -C ./Putty
+./dockcross-linux-x86 make -f ./UNIXx86/Makefile  -C ./Putty
---
+++ init-dockcross-compilers.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+docker run --rm dockcross/linux-x64 > ./dockcross-linux-x64
+docker run --rm dockcross/linux-x86 > ./dockcross-linux-x86
+docker run --rm dockcross/web-wasm > ./dockcross-web-wasm
+chmod +x ./dockcross-linux-x64
+chmod +x ./dockcross-linux-x86
+chmod +x ./dockcross-web-wasm
