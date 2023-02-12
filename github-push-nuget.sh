9b77836af388688795bef13a60fd85a6b304b988
Rytis Petronis <petronisrytis@gmail.com>
2023-02-12 13:30:02 +0000

    add github package push script

---
+++ github-push-nuget.sh
@@ -0,0 +1,2 @@
+﻿dotnet nuget add source --name github "https://nuget.pkg.github.com/rytisgit/index.json"
+dotnet nuget push ".\PuttyXtermEmulator.1.0.0.nupkg"  --api-key GithubKeyHere --source "github"
